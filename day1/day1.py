b9a73a64a8501cb7c90d8109a7a0716fbf715643
Victoria DeRidder <vleighfisher@gmail.com>
2024-12-02 12:07:28 -0500

    there was a part 2!!!

--- day1/day1.py
+++ day1/day1.py
@@ -12,6 +12,17 @@ for i in input:
 arr1.sort()
 arr2.sort()
 
+# part 1 answer = 2066446
 for idx, i in enumerate(arr1):
     total += abs(int(i) - int(arr2[idx]))
     count += 1
+
+# part 2 answer = 24931009
+arr1_set = set(arr1)
+similarity_score = 0
+for i in arr1_set:
+    for j in arr2:
+        if j == i:
+            similarity_score += int(i)
+
+print(similarity_score)
